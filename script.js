341148239697be29be719092f9b9469ccb9707b5
Leonardo C Araújo <ogbeni-leonardo@outlook.com>
2022-04-24 17:53:32 -0300

    Resolução dos requisitos 03 e 04

--- script.js
+++ script.js
@@ -2,3 +2,41 @@
 function byId(id) {
   return document.getElementById(id);
 }
+
+// Retorna elemento(s) segundo seu seletor
+function qsa(selector) {
+  return document.querySelectorAll(selector);
+}
+
+// Cria um novo elemento e o retorna
+function createElement(elementName) {
+  return document.createElement(elementName);
+}
+
+// Limpar todos os elementos filhos de um elemento pai segundo o seletor
+function cleanChildren(selector) {
+  const childElements = qsa(selector);
+  for (let index = 0; index < childElements.length; index += 1) {
+    childElements[index].remove();
+  }
+}
+
+// Pega os valores inserido no input e os aplica ao parágrafo
+function getInputValue() {
+  const textInput = byId('carta-texto').value.trim();
+  if (textInput.length === 0) return;
+
+  const paragraph = byId('carta-gerada');
+  cleanChildren('#carta-gerada span');
+
+  const textInputSplited = textInput.split(' ');
+  for (let index = 0; index < textInputSplited.length; index += 1) {
+    const newSpan = createElement('span');
+    newSpan.innerText = textInputSplited[index];
+
+    paragraph.appendChild(newSpan);
+  }
+}
+
+const criarCarta = byId('criar-carta');
+criarCarta.onclick = getInputValue;
